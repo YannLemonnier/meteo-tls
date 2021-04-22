d67708962a1e5b767aecdbd401bc1188405dec66
yann Lemonnier <yann.lemonnier@gmail.com>
2021-04-22 18:39:20 +0200

    Refactor and test ingest_station_description.

--- ingest/main.py
+++ ingest/main.py
@@ -1,21 +1,6 @@
-from ingest.convert_schema import ConvertSchema
-from ingest.import_gs_file_in_bq import ImportGsFileInBq
-from ingest.import_toulouse_dataset import ImportToulouseDataset
+from ingest.ingest_station_description import ingest_station_description
 
 from ingest.initiate_dataset import initiate_dataset
-
-
-def ingest_station_description(dataset: str):
-    table = 'stations-meteo-en-place'
-
-    stations = ImportToulouseDataset(table).upload()
-    uri = f'gs://{stations.bucket.name}/{stations.name}'
-
-    my_schema = ConvertSchema(table).bq_schema
-    import_session = ImportGsFileInBq(dataset, table, my_schema)
-
-    import_session.load(uri)
-
 
 if __name__ == '__main__':
     stage_dataset = initiate_dataset()
---
+++ tests/ingest/test_ingest_station_description.py
@@ -0,0 +1,28 @@
+from unittest.mock import patch
+
+import pytest
+
+from ingest.ingest_station_description import ingest_station_description
+
+
+@pytest.fixture
+def mock_ingest_station_description():
+    with patch('ingest.import_toulouse_dataset.ImportToulouseDataset.upload') as upload_url:
+        with patch('ingest.import_gs_file_in_bq.ImportGsFileInBq.load') as load_table:
+            ingest_station_description('fake_dataset')
+
+            yield upload_url, load_table
+
+
+def test_upload_url(mock_ingest_station_description):
+    upload_url, load_session = mock_ingest_station_description
+    assert upload_url.called
+
+
+def test_load_table(mock_ingest_station_description):
+    upload_url, load_session = mock_ingest_station_description
+    assert load_session.called
+
+
+if __name__ == '__main__':
+    pytest.main()
