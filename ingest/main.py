3d93cab890ae5023b9d8d4e1f56864c4c1a66b6c
yann Lemonnier <yann.lemonnier@gmail.com>
2021-04-24 21:21:26 +0200

    Try handler for ingest

--- ingest/main.py
+++ ingest/main.py
@@ -1,7 +1,18 @@
+from flask import Flask
+
 from ingest.ingest_station_description import ingest_station_description
 
 from ingest.initiate_dataset import initiate_dataset
 
-if __name__ == '__main__':
+app = Flask(__name__)
+
+
+@app.route('/')
+def start_data_ingestion():
     stage_dataset = initiate_dataset()
     ingest_station_description(stage_dataset)
+    return 'Start data ingestion'
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
