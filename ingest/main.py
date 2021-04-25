651fe96ecf923b1d3f66237799e376c38e653747
yann Lemonnier <yann.lemonnier@gmail.com>
2021-04-25 21:46:42 +0200

    Adapt ingest/main.py for pub/sub trigger

--- ingest/main.py
+++ ingest/main.py
@@ -2,6 +2,11 @@ from ingest.ingest_station_description import ingest_station_description
 
 from ingest.initiate_dataset import initiate_dataset
 
-if __name__ == '__main__':
+
+def main(data, context):
     stage_dataset = initiate_dataset()
     ingest_station_description(stage_dataset)
+
+
+if __name__ == "__main__":
+    main('data', 'context')
