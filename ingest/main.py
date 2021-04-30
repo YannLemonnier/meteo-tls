6e73018b60277360db46a6be5f92e727d6e176dc
yann Lemonnier <yann.lemonnier@gmail.com>
2021-04-30 16:00:42 +0200

    Deploy ingest through flask route in default app

--- ingest/main.py
+++ ingest/main.py
@@ -3,11 +3,11 @@ from ingest.ingest_to_bigquery import ingest_station_description, ingest_station
 from ingest.initiate_dataset import initiate_dataset
 
 
-def main(data, context):
+def start_ingest():
     stage_dataset = initiate_dataset()
     ingest_station_description(stage_dataset)
     ingest_station_data(stage_dataset)
 
 
 if __name__ == "__main__":
-    main('data', 'context')
+    start_ingest()
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from ingest.main import main
+from flask import request
+
+from ingest.main import start_ingest
 
 from visualize.stations import stations_map
 
@@ -25,10 +27,29 @@ dash_app.layout = html.Div(children=[
 ])
 
 
+# Define the decorator to protect your end points
+def validate_cron_header(protected_function):
+    def cron_header_validator_wrapper(*args, **kwargs):
+        header = request.headers.get('X-Appengine-Cron')
+
+        if not header:
+            # here you can raise an error, redirect to a page, etc.
+            return '403'
+
+        # Run and return the protected function
+        return protected_function(*args, **kwargs)
+
+    # The line below is necessary to allow the use of the wrapper on multiple endpoints
+    # https://stackoverflow.com/a/42254713
+    cron_header_validator_wrapper.__name__ = protected_function.__name__
+    return cron_header_validator_wrapper
+
+
 @app.route('/tasks/ingest')
+@validate_cron_header
 def start():
     """Return a friendly HTTP greeting."""
-    print('data')
+    start_ingest()
     return '200'
 
 
