4b9d3f9829779da535be9c364eb7cbf68acbccbf
elska <yann.lemonnier@gmail.com>
2021-04-30 23:03:16 +0200

    filter out station data with uncommon schema

--- ingest/ingest_to_bigquery.py
+++ ingest/ingest_to_bigquery.py
@@ -1,5 +1,7 @@
 from typing import Union, List
 
+import pandas
+
 from ingest.convert_schema import ConvertSchema
 from ingest.import_gs_file_in_bq import ImportGsFileInBq
 from ingest.import_toulouse_dataset import ImportToulouseDataset
@@ -8,24 +10,37 @@ from ingest.queries import table_to_df
 
 def ingest_station_description(dataset: str):
     table = 'stations-meteo-en-place'
-    ingest_open_data_in_dataset_table_given_schema(open_dataset=table, dataset=dataset, table=table, schema=table)
+    file_blob = ImportToulouseDataset(table).upload()
+    uri = f'gs://{file_blob.bucket.name}/{file_blob.name}'
+
+    load_in_bigquery(uris=uri, dataset=dataset, table=table, schema=table)
 
 
 def ingest_station_data(dataset: str):
     stations_info = table_to_df('stations-meteo-en-place')
     list_of_datasets = stations_info['id_nom'].to_list()
 
-    table = 'stations'
-    schema = '58-station-meteo-toulouse-fondeyre'
+    table = 'raw_stations'
+    schema = '14-station-meteo-toulouse-centre-pierre-potier'
+    uris = load_in_gcs(list_of_datasets)
 
-    ingest_open_data_in_dataset_table_given_schema(list_of_datasets, dataset, table, schema)
+    station_with_most_common_schema = get_most_common_schema_station_list(uris)
+
+    load_in_bigquery(station_with_most_common_schema, dataset, table, schema)
 
 
-def ingest_open_data_in_dataset_table_given_schema(open_dataset: Union[str, List[str]], dataset: str, table: str,
-                                                   schema: str):
+def load_in_bigquery(uris: Union[str, List[str]], dataset: str, table: str,
+                     schema: str):
+    my_schema = ConvertSchema(schema).bq_schema
+    import_session = ImportGsFileInBq(dataset, table, my_schema)
+
+    print('Import in BigQuery')
+    import_session.load(uris)
+
+
+def load_in_gcs(open_dataset):
     if isinstance(open_dataset, str):
         open_dataset = [open_dataset]
-
     uris = []
     for open_data in open_dataset:
         try:
@@ -35,12 +50,22 @@ def ingest_open_data_in_dataset_table_given_schema(open_dataset: Union[str, List
             uris.append(uri)
         except ValueError:
             print(f'failed')
+    return uris
 
-    my_schema = ConvertSchema(schema).bq_schema
-    import_session = ImportGsFileInBq(dataset, table, my_schema)
 
-    print('Import in BigQuery')
-    import_session.load(uris)
+def get_most_common_schema_station_list(uris_station: List[str]) -> List[str]:
+    common_list = []
+    for station in uris_station:
+        try:
+            header = pandas.read_csv(station, nrows=1).columns.to_list()
+        except FileNotFoundError:
+            pass
+
+        # Keep only most common header
+        if len(header) == 15:
+            common_list.append(station)
+
+    return common_list
 
 
 if __name__ == '__main__':
