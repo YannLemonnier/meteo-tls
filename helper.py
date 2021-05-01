023d257df441838e6cba24b447d353072e09a313
elska <yann.lemonnier@gmail.com>
2021-05-01 14:58:02 +0200

    Change instance and scaling to run long running process

--- helper.py
+++ helper.py
@@ -7,7 +7,7 @@ import requests
 
 def staying_alive(url: str, wait_seconds: float, quit_event: multiprocessing.Event):
     while not quit_event.is_set():
-        print('Ha, Ha, Ha, Ha! Staying alive')
+        print(f'Ha! Ha! Ha! Ha! Staying {url} alive')
         requests.get(url)
         time.sleep(wait_seconds)
 
