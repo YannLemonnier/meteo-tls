e6abbeccadd5835152e82cd626c81dc9369a2d38
elska <yann.lemonnier@gmail.com>
2021-05-03 00:10:37 +0200

    Strip down test_stations.py

--- tests/visualize/test_stations.py
+++ tests/visualize/test_stations.py
@@ -1,7 +1,4 @@
-import pandas as pd
 import pytest
-
-from unittest.mock import patch
 
 from plotly.graph_objs import Figure
 
@@ -9,10 +6,7 @@ from visualize.stations import stations_map
 
 
 def test_stations_map():
-    with patch('ingest.queries.table_to_df') as df:
-        df.return_value = pd.DataFrame([[1.42, 43.6, 135, 'factice']],
-                                       columns=['longitude', 'latitude', 'altitude', 'id_nom'])
-        assert isinstance(stations_map(), Figure)
+    assert isinstance(stations_map(), Figure)
 
 
 if __name__ == '__main__':
