58f115c00d9848b597b95b51b2cf2f14bd6c7d23
elska <yann.lemonnier@gmail.com>
2021-05-02 02:26:50 +0200

    Use average temperature as color for stations markers

--- ingest/queries.py
+++ ingest/queries.py
@@ -16,3 +16,24 @@ def table_to_df(table_name: str) -> pandas.DataFrame:
     results = query_job.result()  # Waits for job to complete.
 
     return results.to_dataframe()
+
+
+def get_temp_to_df(station_table: str, data_table: str) -> pandas.DataFrame:
+    client = bigquery.Client()
+    query_job = client.query(
+        f"""
+        with temp_data as (
+            SELECT id, ROUND(AVG(temperature_en_degre_c),1) as temp, COUNT(*) as cnt
+            FROM `{client.project}.dataset.{data_table}`
+            group by id) 
+        
+        select id_nom, temp_data.temp, stations.longitude, stations.latitude
+        FROM `{client.project}.dataset.{station_table}` as stations
+        join temp_data on stations.id_numero = temp_data.id
+        WHERE cnt > 40000
+        """
+    )
+
+    results = query_job.result()  # Waits for job to complete.
+
+    return results.to_dataframe()
--- visualize/stations.py
+++ visualize/stations.py
@@ -1,15 +1,15 @@
 import plotly.express as px
 from plotly.graph_objs import Figure
 
-from ingest.queries import table_to_df
+from ingest.queries import get_temp_to_df
 
 
 def stations_map() -> Figure:
-    stations_info = table_to_df('stations-meteo-en-place')
+    stations_info = get_temp_to_df('stations-meteo-en-place', 'stations')
 
-    figure = px.scatter_mapbox(stations_info, lat="latitude", lon="longitude", color="altitude",
+    figure = px.scatter_mapbox(stations_info, lat="latitude", lon="longitude", color="temp",
                                hover_name="id_nom",
-                               color_continuous_scale="earth", size_max=15, zoom=12)
+                               color_continuous_scale="bluered", size_max=15, zoom=12)
 
     figure.update_layout(mapbox_style="carto-darkmatter",
                          paper_bgcolor="rgba(0,0,0,0)",
