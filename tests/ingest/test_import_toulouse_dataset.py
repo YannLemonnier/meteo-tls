65326fd7af5fc2efc61bcaa164ffdd43bff425cc
elska <yann.lemonnier@gmail.com>
2021-05-05 22:58:40 +0200

    test_import_toulouse_dataset.py: Patch several access to gcloud ressources that may not exist when initiating a new instance of the application

--- tests/ingest/test_import_toulouse_dataset.py
+++ tests/ingest/test_import_toulouse_dataset.py
@@ -1,9 +1,10 @@
+import io
 from unittest.mock import patch
 from urllib.parse import urlparse
 
 import pytest
 from google.api_core.exceptions import NotFound
-from google.cloud.storage import Blob
+from google.cloud.storage import Blob, Bucket
 
 from ingest.import_toulouse_dataset import ImportToulouseDataset
 
@@ -20,7 +21,7 @@ class TestImportDatasetFile:
     def test_error_when_url_is_not_reachable(self):
         with patch('google.cloud.storage.client.Client.get_bucket'):
             with pytest.raises(ValueError, match='Issues when reaching dataset url. Please check dataset name'):
-                ImportToulouseDataset('factice_dataset', 'meteo-tls').upload()
+                ImportToulouseDataset('factice_dataset', 'factice_project').upload()
 
     def test_error_with_invalid_project(self):
         with patch('google.cloud.storage.client.Client.get_bucket') as get_bucket:
@@ -28,15 +29,22 @@ class TestImportDatasetFile:
             with pytest.raises(ValueError, match='Issues when reaching project bucket. Please check project name'):
                 ImportToulouseDataset('stations-meteo-en-place', 'factice_project').upload()
 
-    def test_call_upload_from_string(self):
-        with patch('google.cloud.storage.blob.Blob.upload_from_string') as upload:
-            ImportToulouseDataset('stations-meteo-en-place', 'meteo-tls').upload()
-            assert upload.called
+    @pytest.fixture
+    def setup(self):
+        with patch('google.cloud.storage.client.Client.get_bucket') as get_bucket:
+            with patch('google.cloud.storage.blob.Blob.upload_from_string') as upload:
+                import_session = ImportToulouseDataset('fake-dataset')
+                get_bucket.return_value = Bucket(import_session.storage_client)
+                with patch('ingest.import_toulouse_dataset.ImportToulouseDataset.dataset_stream') as dataset_stream:
+                    dataset_stream.return_value = io.StringIO()
+                    yield upload, import_session.upload()
 
-    def test_call_upload_return_blob(self):
-        with patch('google.cloud.storage.blob.Blob.upload_from_string'):
-            result = ImportToulouseDataset('stations-meteo-en-place', 'meteo-tls').upload()
+    def test_call_upload_from_string(self, setup):
+        upload, _ = setup
+        assert upload.called
 
+    def test_call_upload_return_blob(self, setup):
+        _, result = setup
         assert isinstance(result, Blob)
 
 
